dea8db48ceab633575ac7dbccd6e7a088a16f91b
vasilyukleonid <47222462+vasilyukleonid@users.noreply.github.com>
2019-02-09 17:17:25 +0000

    Delete SQLQuery002.sql

--- SQLQuery002.sql
+++
@@ -1,6 +0,0 @@
-SELECT SupplierName, SupplierID
-FROM [Purchasing].[Suppliers]
-EXCEPT
-SELECT S.SupplierName, S.SupplierID
-FROM [Purchasing].[Suppliers] as S RIGHT JOIN [Purchasing].[SupplierTransactions] as ST
-ON S.SupplierID = ST.SupplierID

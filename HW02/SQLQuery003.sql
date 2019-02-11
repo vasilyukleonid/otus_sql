7f6ea0d484ea902493976ddd33c3968c4703038c
vasilyukleonid <vasiluk78@gmail.com>
2019-02-11 21:28:38 +0300

    Update SQLQuery003.sql

--- HW02/SQLQuery003.sql
+++ HW02/SQLQuery003.sql
@@ -8,3 +8,4 @@ FROM [WideWorldImporters].[Sales].[Orders] O
 WHERE (OL.UnitPrice > 100 OR OL.Quantity > 20)
 	AND O.PickingCompletedWhen IS NOT NULL
 ORDER BY Quart, O.OrderDate
+	OFFSET 1000 ROWS FETCH NEXT 100 ROWS ONLY; -- ѕропуск первых 1000 строк и вывод следующих 100

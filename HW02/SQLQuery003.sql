fa8ced6ee4b4f5eea78ba167738d24ae8ce8c1c1
vasilyukleonid <vasiluk78@gmail.com>
2019-02-12 16:10:49 +0300

    Update SQLQuery003.sql

--- HW02/SQLQuery003.sql
+++ HW02/SQLQuery003.sql
@@ -1,6 +1,12 @@
 SELECT O.OrderDate
 	,O.OrderID
 	,DATEPART(quarter, O.OrderDate) AS Quart
+	,DATENAME(month, O.OrderDate) AS Month
+	,"Third" = CASE 
+		WHEN DATEPART(month,O.OrderDate) >= 1 AND DATEPART(month,O.OrderDate) <= 4 THEN 'First'
+		WHEN DATEPART(month,O.OrderDate) >= 5 AND DATEPART(month,O.OrderDate) <= 8 THEN 'Second'
+		WHEN DATEPART(month,O.OrderDate) >= 9 AND DATEPART(month,O.OrderDate) <= 12 THEN 'Third'
+		END
 	,OL.UnitPrice
 	,OL.Quantity
 FROM [WideWorldImporters].[Sales].[Orders] O
@@ -8,4 +14,4 @@ FROM [WideWorldImporters].[Sales].[Orders] O
 WHERE (OL.UnitPrice > 100 OR OL.Quantity > 20)
 	AND O.PickingCompletedWhen IS NOT NULL
 ORDER BY Quart, O.OrderDate
-	OFFSET 1000 ROWS FETCH NEXT 100 ROWS ONLY; -- Skip first 1000 rwas and shaw next 100
+--	OFFSET 1000 ROWS FETCH NEXT 100 ROWS ONLY; -- Skip first 1000 rwas and shaw next 100

67b5e47a742d4cacaf1a0487c432982f5f3d9f9d
vasilyukleonid <vasiluk78@gmail.com>
2019-02-18 17:52:11 +0300

    Update SQLQuery002.sql

--- HW04/SQLQuery002.sql
+++ HW04/SQLQuery002.sql
@@ -1,3 +1,9 @@
+-- 1
 SELECT *
   FROM [WideWorldImporters].[Warehouse].[StockItems]
   WHERE UnitPrice = (SELECT MIN (UnitPrice) FROM [WideWorldImporters].[Warehouse].[StockItems])
+
+-- 2?
+SELECT *
+  FROM [WideWorldImporters].[Warehouse].[StockItems]
+  WHERE UnitPrice = (SELECT TOP(1) UnitPrice FROM [WideWorldImporters].[Warehouse].[StockItems] ORDER BY UnitPrice ASC)

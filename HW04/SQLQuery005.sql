257057886117f8aa1d0019b4ae2387cf6e144ba5
vasilyukleonid <47222462+vasilyukleonid@users.noreply.github.com>
2019-02-26 11:40:19 +0300

    Update SQLQuery005.sql

--- HW04/SQLQuery005.sql
+++ HW04/SQLQuery005.sql
@@ -23,27 +23,3 @@ FROM Sales.Invoices
 ORDER BY TotalSumm DESC
 
 -- выводит ИД счета заказов, дату заказа, продавца, общая сумма которых больше 27000
-
-SELECT Invoices.InvoiceID
-	,Invoices.InvoiceDate
-	,(SELECT People.FullName 
-		FROM Application.People 
-		WHERE People.PersonID = Invoices.SalespersonPersonID) AS SalesPersonName
-	,SalesTotals.TotalSumm AS TotalSummByInvoice
-	,(SELECT SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice) 
-		FROM Sales.OrderLines 
-		WHERE OrderLines.OrderId = 
-			(SELECT Orders.OrderId 
-				FROM Sales.Orders
-				WHERE Orders.PickingCompletedWhen IS NOT NULL	
-					AND Orders.OrderId = Invoices.OrderId)	
-	) AS TotalSummForPickedItems
-FROM Sales.Invoices 
-	JOIN
-	(SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
-		FROM Sales.InvoiceLines
-		GROUP BY InvoiceId
-		HAVING SUM(Quantity*UnitPrice) > 27000
-	) AS SalesTotals
-	ON Invoices.InvoiceID = SalesTotals.InvoiceID
-ORDER BY TotalSumm DESC
